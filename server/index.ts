5715b3df67a71c9c9b6dbd3cc30963826109e01d
Flopi <flopivillagarcia@gmail.com>
2018-10-28 14:50:08 +0100

    refactor index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,16 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+import { createServer } from "http";
+import * as next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, dir: "client" });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
+const boostrap = async () => {
+  await app.prepare();
   createServer(handle).listen(3000, err => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
   });
-});
+};
+
+boostrap();
